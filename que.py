3040cceb325fbf19b156bc59ddad51f32b55991f
andresbuendia2020 <andresbuendia560@gmail.com>
2020-01-22 15:17:04 -0500

    andres cambio de gif a png

--- que.py
+++ que.py
@@ -14,5 +14,9 @@ print ("Que hacen")
 print ("DE AQUI PARA ABAJO DEBEN EDITAR ALGO Y DARLE PUSH DESDE SUS RAMAS")
 =======
 print ("DE AQUI PARA ABAJO DEBEN DITAR ALGO Y DARLE PUSH DESDE SUS RAMAS") 
+<<<<<<< HEAD
 print ("Vale vere que hago")
+=======
+print ("Vale ver que hago")
+>>>>>>> faea02cb028ae27d49453af9156a72296a58c178
 >>>>>>> d117e2b19180b14705e5aad8fbb0c82f71a61515
