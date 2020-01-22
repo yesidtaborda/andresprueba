6d86ee19efaf9ff44cccba99dc0c9600298a9bfa
yesid <laal@gmail.com>
2020-01-22 14:46:39 -0500

    nueva edicion para jonathan busque el cambio

---
+++ que.py
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+=======
+print ("Que hacen") 
+>>>>>>> d117e2b19180b14705e5aad8fbb0c82f71a61515
+print ("Que hacen")
+print ("Que hacen")
+print ("Que hacen")
+print ("Que hacen")
+print ("Que hacen")
+print ("Que hacen")
+print ("Que hacen")
+print ("Que hacen")
+<<<<<<< HEAD
+print ("DE AQUI PARA ABAJO DEBEN EDITAR ALGO Y DARLE PUSH DESDE SUS RAMAS")
+=======
+print ("DE AQUI PARA ABAJO DEBEN DITAR ALGO Y DARLE PUSH DESDE SUS RAMAS") 
+print ("Vale ver que hago")
+>>>>>>> d117e2b19180b14705e5aad8fbb0c82f71a61515
